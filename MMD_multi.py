3010164836ede0da6fc96a9c7cbece6bfb26a60d
c126012u <c126012u@comwork.c.oka-pu.ac.jp>
2017-10-13 16:30:50 +0900

    Create MMD_multi.py

---
+++ MMD_multi.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+参考：http://blog.livedoor.jp/sce_info3-craft/archives/9297948.html
+
+
+#speech.jsonを受け取る用のサーバ
+#class で書くとreturn で値が取得できる? → http://news.mynavi.jp/series/python/033/
+
+
+#scene.jsonを受け取る用のサーバ
+#受け取ったものをreturnしたい
