797eb8225baabf277634b3fa6bdbd6df341e347d
c126012u <c126012u@comwork.c.oka-pu.ac.jp>
2017-10-23 17:05:04 +0900

    Add files via upload

--- MMD.py
+++ MMD.py
@@ -4,48 +4,100 @@
 import json
 import jtalk #OpenJTalk
 import talk #talkAPI
-import #rulematch
+from rule_base import res7
+import socket
+import time
+import sys
 
+host = "127.0.0.1"
+
+#sceneサーバ、ポート番号
+sc_port = 5001
+
+#speechサーバ、ポート番号
+sp_port = 5000
 
 while True:
 
-    '''認識結果のデータを読み込む'''
-    #IDは画像から物体認識や動作認識が終わったあと
-    #指差し情報（座標情報込み？）windowsからubuntuに送られてくる
+	print("認識開始:Enter")
+	trig = sys.stdin.readline()
+	speech = {'sentence1': {'sentence': '', 'score': '0', 'word': [], 'CM': []}, 'sentence2': {'sentence': '', 'score': '0', 'word': [], 'CM': []}, 'sentence3': {'sentence': '', 'score': '0', 'word': [], 'CM': []}, 'sentence4': {'sentence': '', 'score': '0', 'word': [], 'CM': []}, 'sentence5': {'sentence': '', 'score': '0', 'word': [], 'CM': []}, 'sentence6': {'sentence': '', 'score': '0', 'word': [], 'CM': []}, 'sentence7': {'sentence': '', 'score': '0', 'word': [], 'CM': []}, 'sentence8': {'sentence': '', 'score': '0', 'word': [], 'CM': []}, 'sentence9': {'sentence': '', 'score': '0', 'word': [], 'CM': []}, 'sentence10': {'sentence': '', 'score': '0', 'word': [], 'CM': []}}
+	scene = {'':{'location':[],'motion':'','name':[],'confidence':[]} }
+	res = {}
 
-    #sentence.json（ubuntuに保存されているので読み込む）
-    with open('sentence.json', 'r') as f:
-        speech = json.load(f)#辞書型
+	sc_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sc_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	sc_sock.bind((host,sc_port)) #IPとPORTを指定してバインドします
+	sc_sock.listen(1)
+	sc_sock.setblocking(False)
 
-    with open('ID.json','r') as fi:
-        input_scene = json.laod(fi)
+	sp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	sp_sock.bind((host,sp_port)) #IPとPORTを指定してバインドします
+	sp_sock.listen(1)
+	sp_sock.setblocking(False)
 
-    while True:
-	    '''
-    	ルールベース対話にマッチするかどうか
-        IDとsent を入力
-        出力はJSON形式「res.json」(ルールマッチ関数内で保存しておく)
-        {
-   		"response":{
-     		"txt" : "",
-     		"motion" : "",
-     		"ID" : ""
-   		    }
-	    }
-	    この関数の出力は辞書型オブジェクト
-	    '''
-	    #res.jsonの辞書型オブジェクト res を受け取る
+	#認識開始のループ
+	while trig == "\n":
+		try:
+			sc_client, sc_address = sc_sock.accept()
+			scene = sc_client.recv(4096) #ここの時点でbytes
+			scene = scene.decode('utf-8')
+			scene = json.loads(scene)
+			print('Received -> ')
+			print(scene)
 
-	    #resのそれぞれの値が無い時、APIに入力音声テキストを入れる
-	    if res["response"]["txt"]==res["response"]["motion"]==res["response"]["name"]=="":
-    		res["response"]["txt"] = talk.chat(speech["sentence1"]["sentence"])
+		except BlockingIOError:
+			time.sleep(0.1)
+			print("sceneなし")
 
-    		#res.jsonにAPIの出力（テキスト）を書き込む
-    		with open('res.json','w') as fw:
-        		json.dump(res, fw, indent=4,ensure_ascii=False)
+		try:
+			sp_client, sp_address = sp_sock.accept()
+			speech = sp_client.recv(4096) #ここの時点でbytes
+			speech = speech.decode('utf-8')
+			speech = json.loads(speech)
+			print('Received -> ')
+			print(speech)
 
+		except BlockingIOError:
+			time.sleep(0.1)
+			print("speechなし")
 
- 	     #ロボット動作
-	     #音声合成
-	     jtalk.say(res["response"]["txt"])
-	     break
+		#ルールマッチプログラムへ入力	
+		try:
+			res = res7.main(speech, scene)
+		except UnboundLocalError:
+			time.sleep(0.1)
+			pass
+		except TypeError:
+			time.sleep(0.1)
+			pass
+
+		if res == {}: #音声なし、ルールマッチなし
+			print("0.1秒待つ")
+			time.sleep(0.1)
+			pass
+		#音声なし、ルールマッチあり
+		elif res["ID"]!="" and res["RESPONSE"]=="" and res["ORDER"]!="":
+			print("ロボット")
+			trig = "" ##認識開始前にもどる
+			break
+		elif res["RESPONSE"]==res["ID"]==res["ORDER"]=="":#音声有り、ルールマッチなし
+			res["RESPONSE"] = talk.chat(res["input_txt"])
+			print(res)
+			jtalk.say(res["RESPONSE"])
+			trig = "" ##認識開始前にもどる
+			break
+
+		else:
+			#書き込み
+			print(res)
+			with open('res.json','w') as fw:
+				json.dump(res, fw, indent=4,ensure_ascii=False)
+			jtalk.say(res["RESPONSE"])
+			trig = "" ##認識開始前にもどる
+			break
+
+	#sc_client.close() #NameError: name 'sc_client' is not defined
+	#sp_client.close()
+	#sc_client.detach()
